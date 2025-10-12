0110bb728e86e92d178158d5632d65db0bd4104f
butuz0 <y.o.oryshchenko@gmail.com>
2025-10-12 13:14:20 +0300

    updated backend startup script wait_for_db command, changed env.template placeholder values

--- backend/apps/common/management/commands/wait_for_db.py
+++ backend/apps/common/management/commands/wait_for_db.py
@@ -1,7 +1,9 @@
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
-from psycopg2 import OperationalError as Psycopg2OpError
+from psycopg2 import connect, OperationalError as Psycopg2OpError
 import time
+import sys
+import os
 
 
 class Command(BaseCommand):
@@ -9,12 +11,29 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         db_up = False
-        while not db_up:
-            try:
-                self.check(databases=['default'])
-                db_up = True
-            except (Psycopg2OpError, OperationalError):
-                self.stdout.write('Waiting for database to become available...')
-                time.sleep(1)
+        retries_left = int(os.environ.get('MAX_DB_CONN_RETRIES', 30))
+        sleep_time = int(os.environ.get('DB_WAIT_SLEEP', 1))
 
-        self.stdout.write(self.style.SUCCESS('Database is available!'))
+        while not db_up and retries_left:
+            try:
+                connect(
+                    dbname=os.environ.get('DB_NAME'),
+                    user=os.environ.get('DB_USER'),
+                    password=os.environ.get('DB_PASSWORD'),
+                    host=os.environ.get('DB_HOST'),
+                    port=os.environ.get('DB_PORT')
+                )
+                db_up = True
+            except (Psycopg2OpError, OperationalError) as e:
+                if 'database' in str(e) or 'db' in str(e):
+                    self.stderr.write('Waiting for database to become available...\n')
+                    retries_left -= 1
+                    time.sleep(sleep_time)
+                else:
+                    raise
+
+        if not retries_left:
+            self.stdout.write('Database unavailable after waiting')
+            sys.exit(1)
+        else:
+            self.stdout.write(self.style.SUCCESS('Database is available!'))
--- backend/scripts/run.sh
+++ backend/scripts/run.sh
@@ -4,6 +4,6 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-python manage.py wait_for_db
-python manage.py migrate --no-input
+python manage.py wait_for_db || exit 1
+python manage.py migrate --no-input || exit 1
 exec python manage.py runserver 0.0.0.0:8000
