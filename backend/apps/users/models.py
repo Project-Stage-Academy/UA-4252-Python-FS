dc7c593791bf4e1da83da1c6be53077611985e26
butuz0 <y.o.oryshchenko@gmail.com>
2025-10-13 19:37:51 +0300

    add custom user model

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
+import uuid
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Custom Django User model without username"""
+    username = None
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True, db_index=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
