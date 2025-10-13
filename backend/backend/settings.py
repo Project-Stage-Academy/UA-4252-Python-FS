edc22ee3a9e97603c2e3809fd52a1a7bd0f0fa3f
Tonnnya <53794265+Tonnnya@users.noreply.github.com>
2025-10-13 23:15:57 +0300

    fix: updated wait_for_db.py and settings for Docker

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -95,12 +95,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT', 5432),
-    }
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT', 5432),}
 }
 
 # Password validation
