aba6ec802dc5d80446d063ff27041ac13d7a64dc
butuz0 <y.o.oryshchenko@gmail.com>
2025-10-13 10:33:46 +0300

    added explicit db connection check to healthcheck

--- backend/apps/common/views.py
+++ backend/apps/common/views.py
@@ -1,10 +1,31 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.db import connections
+from django.db.utils import OperationalError
 
 
 class GetHealth(APIView):
     """Simple health-check endpoint to check health."""
 
     def get(self, request):
-        return Response({'status': 'ok'}, status=status.HTTP_200_OK)
+        db_conn = connections['default']
+        try:
+            db_conn.ensure_connection()
+            db_conn.cursor().execute('SELECT 1;')
+        except OperationalError:
+            return Response(
+                {
+                    'status': 'unhealthy',
+                    'db': 'unreachable',
+                },
+                status=status.HTTP_503_SERVICE_UNAVAILABLE
+            )
+
+        return Response(
+            {
+                'status': 'ok',
+                'db': 'connected',
+            },
+            status=status.HTTP_200_OK
+        )
