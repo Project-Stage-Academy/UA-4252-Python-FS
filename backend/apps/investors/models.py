5278f5a488449d2f06d162970cf0d895a4520a78
Roman <cisinskijroman@gmail.com>
2025-10-29 22:33:45 +0200

    removing unnecessary import

--- backend/apps/investors/models.py
+++ backend/apps/investors/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
-from apps.projects.models import Project
 import uuid
 
 from apps.common.models import TimeStampedModel
