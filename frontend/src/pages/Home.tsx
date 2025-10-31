29f4b3c033c9c8d2031b82f30c3c12ecc884f9a8
Someone Account <158297903+Yenot52@users.noreply.github.com>
2025-10-31 14:31:39 +0200

    Fix Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,6 @@
-// src/pages/Home.tsx
 import React from 'react';
 
 const Home: React.FC = () => {
-  // Тепер Home нічого не рендерить крім свого контенту,
-  // і WhyWorthGrid не дублюється
   return null; 
 };
 
