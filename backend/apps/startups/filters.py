de63d1b665bb77dde455a782547bd56668222277
butuz0 <y.o.oryshchenko@gmail.com>
2025-11-03 10:51:50 +0200

    add gin index for tags, update tag filter, remove password from user fixtures

--- backend/apps/startups/filters.py
+++ backend/apps/startups/filters.py
@@ -8,11 +8,11 @@ class StartupFilter(django_filters.FilterSet):
         field_name="company_name",
         lookup_expr="icontains"
     )
-    tag = django_filters.CharFilter(
-        field_name="tags",
-        lookup_expr="icontains",
-    )
+    tag = django_filters.CharFilter(method="filter_tag")
 
     class Meta:
         model = StartupProfile
         fields = ["company_name", "tag"]
+
+    def filter_tag(self, queryset, name, value):
+        return queryset.filter(tags__overlap=[value])
