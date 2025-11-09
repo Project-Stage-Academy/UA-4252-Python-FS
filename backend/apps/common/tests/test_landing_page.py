b542498d322475e636eb29a347be39d7256a3a41
Roman <cisinskijroman@gmail.com>
2025-11-09 16:16:07 +0200

    fix ci2

--- backend/apps/common/tests/test_landing_page.py
+++ backend/apps/common/tests/test_landing_page.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from django.urls import reverse
 
+
 class LandingContentAPITests(APITestCase):
 
     def setUp(self) -> None:
