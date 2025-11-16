b5204b9d485cca04621a3483872a03abe25cd527
Tonnnya <53794265+Tonnnya@users.noreply.github.com>
2025-11-16 20:32:41 +0200

    fixed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -122,13 +122,13 @@ DATABASES = {
     }
 }
 
-if os.environ.get("USE_SQLITE_FOR_TESTS", "").lower() in {"1", "true", "yes"}:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+# if os.environ.get("USE_SQLITE_FOR_TESTS", "").lower() in {"1", "true", "yes"}:
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.sqlite3",
+#             "NAME": BASE_DIR / "db.sqlite3",
+#         }
+#     }
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
@@ -241,7 +241,7 @@ ELASTICSEARCH_DSL = {
         "hosts": f"http://{ES_HOST}:{ES_PORT}",
         "basic_auth": (
             os.environ.get("ELASTIC_USERNAME"),
-            os.environ.get("ELASTIC_PASSWORD")
+            os.environ.get("ELASTIC_PASSWORD"),
         ),
     }
 }
