08931768d6d935d91c9ad2a587c1e5561bec30fc
Tonnnya <53794265+Tonnnya@users.noreply.github.com>
2025-11-16 23:23:43 +0200

    added permissions

--- backend/apps/projects/urls.py
+++ backend/apps/projects/urls.py
@@ -1,18 +1,11 @@
 from django.urls import include, path
-from rest_framework_nested import routers
-
-from apps.startups.views import StartupPublicProfileViewSet
+from rest_framework.routers import DefaultRouter
 
 from .views import ProjectViewSet
 
-router = routers.SimpleRouter()
-router.register(r"startups", StartupPublicProfileViewSet, basename="startups")
-router.register(r"projects", ProjectViewSet, basename="projects")
-
-projects_router = routers.NestedSimpleRouter(router, r"startups", lookup="startup")
-projects_router.register(r"projects", ProjectViewSet, basename="startup-projects")
+router = DefaultRouter()
+router.register(r"", ProjectViewSet, basename="projects")
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('', include(projects_router.urls)),
 ]
